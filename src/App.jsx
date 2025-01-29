d9e086d53c4a2a42e5f67d02115c6f9ee1a82880
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2025-01-29 09:34:34 -0300

    Melhorias

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,18 @@ import Input from './components/Input'
 import useCounter from './hooks/useCounter';
 import styles from './components/Button.module.css'
 
-function App() {
-  console.log('app');
-  const {value, addValue, delValue, amountPlus, amountRemove} = useCounter()
-  return (
-    <>
-      <Input type="number" name="number" id="number" val={value}/>
-      <br />
-      <Button buttonName="Plus" style={`${styles.button} ${styles.plus}`} onClick={() => addValue()} />
-      <Button buttonName="Minus" style={`${styles.button} ${styles.minus}`} onClick={() => delValue()} />
-      <Button buttonName="Add 5" style={`${styles.button} ${styles.del5}`} onClick={() => amountPlus(5)} />
-      <Button buttonName="Del 5" style={`${styles.button} ${styles.add5}`} onClick={() => amountRemove(5)} />
-    </>
-  );
+const App = () => {
+   const { value, addValue, delValue, amountPlus, amountRemove } = useCounter()
+   
+   return <div id='container'>
+      <Input type="number" name="number" id="number" val={value} />
+      <div>
+         <Button buttonName="Plus" style={`${styles.button} ${styles.plus}`} onClick={() => addValue()} />
+         <Button buttonName="Minus" style={`${styles.button} ${styles.minus}`} onClick={() => delValue()} />
+         <Button buttonName="Add 5" style={`${styles.button} ${styles.del5}`} onClick={() => amountPlus(5)} />
+         <Button buttonName="Del 5" style={`${styles.button} ${styles.add5}`} onClick={() => amountRemove(5)} />
+      </div>
+   </div>
 }
 
 export default App;
